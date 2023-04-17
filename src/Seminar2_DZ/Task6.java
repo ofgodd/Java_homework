517d66ec4379e11d172de95f91a92a40bddfba9d
Arkady <djony23rus@mail.ru>
2023-04-18 01:03:35 +0200

    Сделал, но с подсказкой.

---
+++ src/Seminar2_DZ/Task6.java
@@ -0,0 +1,30 @@
+//2. Дана строка json:
+//        [{"фамилия":"Иванов","оценка":"5","предмет":"Математика"},
+//        {"фамилия":"Петрова","оценка":"4","предмет":"Информатика"}, {"фамилия":"Краснов","оценка":"5","предмет":"Физика"}]
+//        Написать метод, который распарсит этот текст и, используя StringBuilder, создаст строки вида:
+//        Студент [фамилия] получил [оценка] по предмету [предмет]. Например:
+//        Студент Иванов получил 5 по предмету Математика.
+//        Студент Петрова получил 4 по предмету Информатика.
+//        Студент Краснов получил 5 по предмету Физика.
+package Seminar2_DZ;
+
+public class Task6 {
+
+    public static StringBuilder PrintLine(String line) {
+        String line1 = line.replace("{", "");
+        String line2 = line1.replace("}", "");
+        String line3 = line2.replaceAll("\"", "");
+        String line4 = line3.replace("[", "");
+        String line5 = line4.replace("]", "");
+        StringBuilder result = new StringBuilder("");
+        String [] arrayData = line5.split(",");
+        String [] listName = {"Студент ", " получил ", " по предмету "};
+        for (int i =0; i < arrayData.length; i++) {
+            String[] arrData = arrayData[i].split(":");
+            result.append(listName[i]);
+            result.append(arrData[1]);
+        }
+        return result;
+    }
+}
+
